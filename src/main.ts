29b02fe412eb8874abcac17f9bfbfdc73430d2dd
crrmacarse <crrmacarse@gmail.com>
2020-11-01 11:26:30 +0800

    feat: integrated security packages

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,40 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as compression from 'compression';
+import * as helmet from 'helmet';
+import * as rateLimit from 'express-rate-limit';
+import { loggerMiddleware } from './middleware/logger.middleware';
+
+const PORT = process.env.PORT || 1111;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  /** Enable CORS */
+  app.enableCors();
+
+  /** Brunte force protection */
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 100, // limit each IP to 100 requests per windowMs
+    }),
+  );
+
+  /**
+   * Secures http security related risk
+   */
+  app.use(helmet());
+
+  /**
+   * Apply gzip compression
+   */
+  app.use(compression())
+
+  /**
+   * Simple logging middleware
+   */
+  app.use(loggerMiddleware);
+
+  await app.listen(PORT);
 }
 bootstrap();
