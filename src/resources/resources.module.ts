d1d7a7b0847d9e47a5116aca55ba2c9ea5ed72d9
crrmacarse <crrmacarse@gmail.com>
2020-11-01 13:09:29 +0800

    feat: added initial project service

--- src/resources/resources.module.ts
+++ src/resources/resources.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common'
 import { ProjectsController } from './projects/projects.controller';
+import { ProjectsService } from './projects/projects.service'
 
 @Module({
     imports: [],
     controllers: [ProjectsController],
+    providers: [ProjectsService],
 })
 export class ResourceModule {}
