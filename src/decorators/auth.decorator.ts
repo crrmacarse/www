97e3e73b7bcdf4fae9db3f2bb3e8c2bcff3b1de6
crrmacarse <crrmacarse@gmail.com>
2020-11-01 15:54:10 +0800

    feat: added type safe acces of auth

---
+++ src/decorators/auth.decorator.ts
@@ -0,0 +1,26 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { roleType } from './roles.decorator';
+
+export type authType = {
+    username: string,
+    roles: roleType[],
+}
+
+/**
+ * Type-safe access of of request context
+ * 
+ * USAGE:
+ * 
+ * @Get()
+ * async findOne(@Auth('username') username: string) {
+ *  console.log(`Hello ${username}`);
+ * }
+ */
+export const Auth = createParamDecorator(
+  (key: keyof authType, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+    const auth: authType = request.user;
+
+    return key ? auth && auth[key] : auth;
+  },
+);
