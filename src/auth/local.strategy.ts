d54b441de0421f74d7253df70b71ccdc749a0c44
crrmacarse <crrmacarse@gmail.com>
2020-11-02 12:05:00 +0800

    feat: added request-scoped-strategy

--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -1,3 +1,4 @@
+import { ContextIdFactory, ModuleRef } from '@nestjs/core';
 import { Strategy } from 'passport-local';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
@@ -5,12 +6,22 @@ import { AuthService } from './auth.service';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
-    constructor(private authService: AuthService) {
-        super();
+    constructor(
+        private moduleRef: ModuleRef
+    ) {
+        super({
+            passReqToCallback: true,
+        });
     }
 
-    async validate(username: string, password: string): Promise<any> {
-        const user = await this.authService.validateUser(username, password);
+    async validate(request: Request, username: string, password: string): Promise<any> {
+        /**
+         * refer: https://docs.nestjs.com/techniques/authentication#request-scoped-strategies
+         */
+        const contextId = ContextIdFactory.getByRequest(request);
+        const authService = await this.moduleRef.resolve(AuthService, contextId);
+
+        const user = await authService.validateUser(username, password);
 
         if (!user) {
             throw new UnauthorizedException();
