7a07c8cd7371cd4a14ebbb66782da3bc90fb0777
crrmacarse <crrmacarse@gmail.com>
2020-11-08 21:43:43 +0800

    feat: initial account module

--- src/resources/resources.module.ts
+++ src/resources/resources.module.ts
@@ -1,8 +1,9 @@
-import { Module } from '@nestjs/common'
+import { Module } from '@nestjs/common';
+import { AccountModule } from './account/account.module';
 import { ProjectModule } from './projects/projects.module'
 import { UsersModule } from './users/users.module';
 
 @Module({
-    imports: [ProjectModule, UsersModule],
+    imports: [AccountModule, ProjectModule, UsersModule],
 })
 export class ResourceModule {}
