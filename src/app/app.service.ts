5d2685f0f9fdfa49933884c53464f2e64ff76533
crrmacarse <crrmacarse@gmail.com>
2020-11-15 19:56:56 +0800

    feat: updated welcome message

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,9 +1,10 @@
 import { Get, Injectable } from '@nestjs/common';
+import welcomeMsg from 'constants/welcome';
 
 @Injectable()
 export class AppService {
   @Get()
   getHello(): string {
-    return 'Hi 👋!';
+    return JSON.parse(JSON.stringify(welcomeMsg));
   }
 }
---
+++ src/constants/welcome.ts
@@ -0,0 +1,43 @@
+export default {
+    'Hi there 👋': 'Welcome to my Personal Server',
+    'title': 'Personal Server',
+    'description': 'A feature-rich NodeJS application where I do experimental stuff',
+    'author': 'crrmacarse',
+    'stack': {
+        'NodeJS': '14.13',
+        'nestjs': '1.4',
+        'MYSQL': '8.0',
+        'pm2': '4.4',
+        'typeorm': '0.2',
+        'typescript': '3.7',
+        'jest': '26.4',
+        'supertest': '4.0'
+    },
+    'features': [
+        'Typesafe codebase',
+        'Clean Structure with ESLint',
+        '100% Test Coverage',
+        'Route Protection',
+        'Caching Mechanism',
+        'Health Checker',
+        'Integrated Scheduler',
+        'Extensie Logging',
+        'CORS enabled',
+        'Brute-force Protection',
+        'Secured from HTTP known risk',
+        'Compression with GZIP'
+    ],
+    'links': [
+        'http://crrmacarse.github.io/',
+        'https://github.com/crrmacarse',
+        'https://stackoverflow.com/users/10030210/crrmacarse',
+        'https://www.upwork.com/freelancers/~0101c083bd752f8e51',
+        'https://www.linkedin.com/in/christian-ryan-r-macarse-692974166/',
+        'https://twitter.com/crrmacarse'
+    ],
+    'faq': {
+        'Repository link': 'https://github.com/crrmacarse/api',
+        'Where can i find the available routes?': 'At the wiki of this repository',
+        'How to get in touch?': 'Email me via crrmacarse@gmail.com'
+    }
+};
