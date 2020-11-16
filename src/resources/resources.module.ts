d97f13f5caf2839b9f0f035e1e43197438f485cd
crrmacarse <crrmacarse@gmail.com>
2020-11-16 12:25:13 +0800

    refactor: deleted sample controller/services

--- src/resources/resources.module.ts
+++ src/resources/resources.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AccountsHttpModule } from './accounts/accounts-http.module';
 import { ProjectsHttpModule } from './projects/projects-http.module';
-import { UsersModule } from './users/users.module';
 
 @Module({
-    imports: [AccountsHttpModule, ProjectsHttpModule, UsersModule],
+    imports: [AccountsHttpModule, ProjectsHttpModule],
 })
 export class ResourceModule {}
