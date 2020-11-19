ec92f7be2651e6e0fca4486468ada1d82682153b
crrmacarse <crrmacarse@gmail.com>
2020-11-19 15:42:37 +0800

    feat: extended automation for release; configured deployment script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
+echo A proud project by crrmacarse(crrmacarse@gmail.com)
+echo
 
-# git hash passed by github
+# extract the package.json version
+PKG_VERSION=`node -p "require('./package.json').version"`
+
+# git hash passed by github. could be use to checkout
+# on specific commit
 echo $1
 
+# make sure to be in master branch
 git checkout master
 
+# get latest changes
 git pull
 
+# install necessary dependencies
 npm install
 
+# build files
 npm run build
 
-pm2 restart api
+# we need to manually set the npm verion as env variable since
+# pm2 is not utilziing npm on start.
+npm_package_version=PKG_VERSION pm2 restart api --update-env
