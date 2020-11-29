ede0b5185eaa1b759da1a347e0e501d4f6478e4b
crrmacarse <crrmacarse@gmail.com>
2020-11-29 13:44:42 +0800

    feat: added cookie parser and date-fns

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,14 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as compression from 'compression';
 import * as helmet from 'helmet';
 import * as rateLimit from 'express-rate-limit';
+import * as cookieParser from 'cookie-parser';
 import { RATE_LIMIT_WINDOW_MS, RATE_LIMIT_MAX } from 'constants/default';
 import { loggerMiddleware } from 'middleware/logger.middleware';;
 import { HttpExceptionFilter } from 'exception/http-exception.filter';
 import { TimeoutInterceptor } from 'interceptors/timeout.interceptor';
 import { AppConfigService } from 'app';
 import { MainModule } from './main.module';
+import { LoggingInterceptor } from 'interceptors/logging.interceptor';
 
 declare const module: any;
 
@@ -23,24 +25,32 @@ async function bootstrap() {
    */
   const appConfig: AppConfigService = app.get('AppConfigService');
 
+  const swaggerTitle = 'Personal API Server';
   const swaggerOptions = new DocumentBuilder()
-    .setTitle('Personal API Server')
+    .setTitle(swaggerTitle)
     .setDescription('https://github.com/crrmacarse')
     .setVersion(`v${process.env.npm_package_version}`)
     .addBearerAuth()
     .build();
 
+  const swagCtrlKeyFormat = (ctrKy: string) => ctrKy.replace('Controller', '').toLocaleLowerCase();
+
   const document = SwaggerModule.createDocument(app, swaggerOptions, {
     operationIdFactory: (
       controllerKey: string,
       methodKey: string
-    ) => methodKey,
+    ) => `${swagCtrlKeyFormat(controllerKey)}-${methodKey}`,
   });
 
   /**
    * Load swagger in /docs route
    */
-  SwaggerModule.setup('docs', app, document);
+  SwaggerModule.setup('docs', app, document, {
+    customSiteTitle: swaggerTitle,
+  });
+
+  /** Enable cookieparser */
+  app.use(cookieParser());
 
   /** Enable CORS */
   app.enableCors();
@@ -84,6 +94,10 @@ async function bootstrap() {
    * Global Interceptors
    */
   app.useGlobalInterceptors(new TimeoutInterceptor());
+
+  if(appConfig.debug) {
+    app.useGlobalInterceptors(new LoggingInterceptor());
+  }
 
   await app.listen(appConfig.port);
 
